0e052861da1ebdf6059e45b1f8a6abd591b6422c
Leland Krych <Leland.Krych@gmail.com>
2017-07-25 10:01:03 -0700

    Create field component for title and tag of blog post in PostsNew Component

--- blog/src/components/posts_new.js
+++ blog/src/components/posts_new.js
@@ -1,13 +1,44 @@
 import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
+
+  renderField(field) {
+    //field.input is an object with event handlers and properties
+    //use spread operator to distribute to input component
+    return (
+      <div className="form-group">
+        <label>{field.label}</label>
+        <input
+          className="form-control"
+          type="text"
+          {...field.input}
+        />
+      </div>
+    );
+  }
+
   render(){
     return(
-      <div>
-        PostsNew!
-      </div>
+      <form>
+        <Field
+          label="Title"
+          name="title"
+          component={this.renderField}
+        />
+
+        <Field
+          label = "Tags"
+          name="tags"
+          component={this.renderField}
+        />
+
+
+      </form>
     );
   }
 }
 
-export default PostsNew;
+export default reduxForm({
+  form: 'PostsNewForm'
+})(PostsNew); //connects form to redux state

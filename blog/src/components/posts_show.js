bc80a865f7b595fa42381ce25ec6ed7d84f8344f
Leland Krych <Leland.Krych@gmail.com>
2017-07-27 10:47:17 -0700

    connect PostsShow to redux state

--- blog/src/components/posts_show.js
+++ blog/src/components/posts_show.js
@@ -10,9 +10,16 @@ class PostsShow extends Component {
   }
 
   render() {
+    const { post } = this.props;
+
+    if(!post){
+      return <div>Loading...</div>;
+    }
     return (
       <div>
-        Posts Show!
+        <h3>{post.title}</h3>
+        <h6>Categories: {post.categories}</h6>
+        <p>{post.content}</p>
       </div>
     );
   }
@@ -22,4 +29,4 @@ function mapStateToProps({ posts }, ownProps) {
   return { post: posts[ownProps.match.params.id]};
 }
 
-export default connect(null, { fetchPost })(PostsShow);
+export default connect(mapStateToProps, { fetchPost })(PostsShow);

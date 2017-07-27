f1f9a36e9205d0dc013e6e8fafc0877026d371d9
Leland Krych <Leland.Krych@gmail.com>
2017-07-27 11:02:27 -0700

    Add callback to deletePost

--- blog/src/reducers/reducer_posts.js
+++ blog/src/reducers/reducer_posts.js
@@ -1,6 +1,6 @@
 import _ from 'lodash';
 
-import { FETCH_POSTS, FETCH_POST } from '../actions';
+import { FETCH_POSTS, FETCH_POST, DELETE_POST } from '../actions';
 
 export default function(state = {}, action) {
   switch (action.type){

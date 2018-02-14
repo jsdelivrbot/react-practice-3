1ce81b08143c879c0faa2927b9e6bfa0a558f616
Leland Krych <Leland.Krych@gmail.com>
2018-02-14 15:18:20 -0800

    display leaflet app using react

--- react-test-leaflet/src/client/app/index.jsx
+++ react-test-leaflet/src/client/app/index.jsx
@@ -1,10 +1,32 @@
-import React from 'react';
-import {render} from 'react-dom';
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import { Map, TileLayer } from 'react-leaflet';
+import MarkerClusterGroup from 'react-leaflet-markercluster';
 
-class App extends React.Component {
+const OMSTiles = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
+const OMSAttr = "&amp;copy <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors";
+const mapCenter = [39.9528, -75.1638];
+const zoomLevel = 12;
+
+export default class App extends Component {
   render() {
-    return <p> Hello React! </p>;
+  
+      return (
+          <div>
+              <Map
+                  center={mapCenter}
+                  zoom={zoomLevel}
+              >
+                  <TileLayer
+                      attribution={OMSAttr}
+                      url={OMSTiles}
+                  />
+              </Map>
+          </div>
+      );
   }
 }
-
-render(<App/>, document.getElementById('app'));
+render(
+  <App />,
+  document.getElementById('mount')
+);
